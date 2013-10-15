c3612271ecf3ea99272b52d02ce0755490e2b5c8
vuphuctho <vuphuctho93@gmail.com>
2013-10-15 23:07:00 +0800

    add function retrieve Hotel from Search

--- website/backend/dtb/retrieveData.php
+++ website/backend/dtb/retrieveData.php
@@ -29,5 +29,74 @@
                               mysql_real_escape_string($email),
                               mysql_real_escape_string($password));
     	}
+
+        public function retrieveHotelListFromSearch($location, $feature1, $feature2,
+                            $room_type1, $room_type2, $room_type3, $room_type4) {
+            // turn $location to lower-case word
+            $location = strtolower($location);
+            
+            // calculate number of features and room type ticked
+            $feature_no = $feature1 + $feature2;
+            $room_type_no = $room_type1 + $room_type2 + $room_type3 + $room_type4;
+
+            // look for checkbox values of features and room types
+            // change them into ID of features or name of room types if ticked
+            if ($feature1 != 0) {
+                $feature1 = 1;
+            }
+            if ($feature2 != 0) {
+                $feature2 = 2;
+            }
+
+            if ($room_type1 != 0) {
+                $room_type1 = "Superior Single";
+            } else {
+                $room_type1 = "";
+            }
+            
+            if ($room_type2 != 0) {
+                $room_type2 = "Superior Double";
+            } else {
+                $room_type2 = "";
+            }
+            
+            if ($room_type3 != 0) {
+                $room_type3 = "Standard Single";
+            } else {
+                $room_type3 = "";
+            }
+            
+            if ($room_type4 != 0) {
+                $room_type4 = "Standard Double";
+            } else {
+                $room_type4 = "";
+            }            
+            // query the hotel list based on requested info
+            if ($feature_no == 0 && $room_type_no == 0) {
+                $query = "SELECT h.* FROM hotel h
+                        WHERE (INSTR(h.address, '{".$location"}') > 0)";
+            } elseif ($feature_no == 0) {
+                $query = "SELECT h.* FROM hotel h
+                        INNER JOIN ROOM_TYPE_TABLE r ON h.hotelID = r.hotelID
+                        WHERE (INSTR(h.address, '{".$location"}') > 0 
+                        AND (r.name = '".$room_type1"' OR r.name = '".$room_type2"' OR
+                             r.name = '".$room_type3"' OR r.name = '".$room_type4"'))";
+            } elseif ($room_type_no == 0) {
+                $query = "SELECT h.* FROM hotel h
+                        INNER JOIN HOTEL_FEATURE_TABLE hf ON h.hotelID = hf.hotelID
+                        WHERE (INSTR(h.address, '{".$location"}') > 0 
+                        AND (hf.featureID = '".$feature1"' OR hf.featureID = '".$feature2"'))";
+            } else {
+                $query = "SELECT h.* FROM hotel h
+                        INNER JOIN HOTEL_FEATURE_TABLE hf ON h.hotelID = hf.hotelID
+                        INNER JOIN ROOM_TYPE_TABLE r ON h.hotelID = r.hotelID
+                        WHERE (INSTR(h.address, '{".$location"}') > 0 
+                        AND (hf.featureID = '".$feature1"' OR hf.featureID = '".$feature2"')
+                        AND (r.name = '".$room_type1"' OR r.name = '".$room_type2"' OR
+                             r.name = '".$room_type3"' OR r.name = '".$room_type4"'))";
+            }
+
+            return mysql_query($query);
+        }
 	}
 ?>
